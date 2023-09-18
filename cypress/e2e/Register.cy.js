e5a6a7bc7615f2deb29c58b6ed85480ee4b368e7
AndreasMunthe <andreasmunthe04@gmail.com>
2023-09-18 16:56:58 +0700

    Fix login Succes

--- cypress/e2e/Register.cy.js
+++ cypress/e2e/Register.cy.js
@@ -1,10 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    /* sanbercode*/
-    /* sanbercode*/
-    /* sanbercode*/
-    /* sanbercode*/
-    /* sanbercode*/
+describe('Register Scenario', () => {
+  beforeEach(() => {
+    cy.visit('demowebshop.tricentis.com')  
+    })
+
+  it('Register ', () => {
+    cy.get('.ico-register').click()
+    cy.get(':nth-child(2) > .form-fields > :nth-child(1) > :nth-child(2)').click()
+    cy.get('#FirstName').type('Test')
+    cy.get('#LastName').type('Dummy')
+    cy.get('#Email').type('TestDummy99@gmail.com')
+    cy.get('#Password').type('Testing12')
+    cy.get('#ConfirmPassword').type('Testing12')
+    cy.get('#register-button').click()
+    cy.get('h1').should('be.visible')
+    cy.get('.page-body > .buttons > .button-1').click()
+    
   })
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('demowebshop.tricentis.com')
-    /* sanbercode*/
-  })
+describe('Login Scenario', () => {
+  beforeEach(() => {
+    cy.visit('demowebshop.tricentis.com')  
+    })
+
+    it('Succes Login', () => {
+      cy.get('.ico-login').click()
+      cy.get('#Email').type('TestDummy99@gmail.com')
+      cy.get('#Password').type('Testing12')
+      cy.get('.reversed').click()
+      cy.get('form > .buttons > .button-1').click()
+      
+ 
+    })
 })
