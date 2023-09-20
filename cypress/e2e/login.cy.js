20b9b300bc58e6402eac88f9c08870a8d41d4349
AndreasMunthe <andreasmunthe04@gmail.com>
2023-09-20 11:15:46 +0700

    Add Github Action

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -9,7 +9,33 @@ describe('Login Scenario', () => {
       cy.get('#Password').type('Testing12')
       cy.get('.reversed').click()
       cy.get('form > .buttons > .button-1').click()
-      
- 
+      cy.get('.header-links > ul > :nth-child(1) > .account').should('be.visible')
+    })
+
+    it('Failed Login | Empty Email & Password', () => {
+      cy.get('.ico-login').click()
+      cy.get('#Email').type(' ')
+      cy.get('#Password').type(' ')
+      cy.get('.reversed').click()
+      cy.get('form > .buttons > .button-1').click()
+      cy.get('.validation-summary-errors').should('be.visible')
+    })
+
+    it('Failed Login | Empty Email', () => {
+      cy.get('.ico-login').click()
+      cy.get('#Email').type('TestDummy99@gmail.com')
+      cy.get('#Password').type(' ')
+      cy.get('.reversed').click()
+      cy.get('form > .buttons > .button-1').click()
+      cy.get('.validation-summary-errors').should('be.visible')
+    })
+
+    it('Failed Login | Empty Password', () => {
+      cy.get('.ico-login').click()
+      cy.get('#Email').type(' ')
+      cy.get('#Password').type('Testing12')
+      cy.get('.reversed').click()
+      cy.get('form > .buttons > .button-1').click()
+      cy.get('.validation-summary-errors').should('be.visible')
     })
 })
